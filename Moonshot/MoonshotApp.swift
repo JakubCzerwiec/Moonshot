e0e739e514505695ef8cc5a7fd5ff73c5b004b37
JakubCzerwiec <jacob.czerwiec@gmail.com>
2024-10-12 22:11:24 +0200

    Initial Commit

---
+++ Moonshot/MoonshotApp.swift
@@ -0,0 +1,17 @@
+//
+//  MoonshotApp.swift
+//  Moonshot
+//
+//  Created by Mój Maczek on 12/10/2024.
+//
+
+import SwiftUI
+
+@main
+struct MoonshotApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
